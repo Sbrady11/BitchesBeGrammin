f480e2268bf1a059223cf0e9b268a348460db068
Rebecca trachtenberg <rebecca.rose1836@yahoo.com>
2017-09-22 09:57:24 -0400

    blah

--- app/assets/javascripts/components/App.jsx
+++ app/assets/javascripts/components/App.jsx
@@ -11,25 +11,14 @@ class App extends React.Component {
   render () {
     const person = {
       avatar: "https://media.giphy.com/media/KI01DytlVPEw8/giphy-facebook_s.jpg",
-      username: "bobo1: ",
-      bio: "HEY NOW",
-      posts: [
-        {
-          pic: 'https://c2.staticflickr.com/4/3559/3446613250_8569b7d582_z.jpg?zz=1',
-          caption: 'Yo this is rad...'
-        },
-        {
-          pic: 'https://c1.staticflickr.com/9/8390/8572035019_0577667f3d_b.jpg',
-          caption: 'Yo this is radder...'
-        }
-      ]
+      username: "bobo1",
+      bio: "HEY NOW"
     }
-
     return (
       <div>
         <Top/>
         <Header data={person}/>
-        <Footer/>
+        <Footer changePage={this.changePage}/>
       </div>
     )
   }
--- app/assets/javascripts/components/Photostream.jsx
+++ app/assets/javascripts/components/Photostream.jsx
@@ -1,5 +1,5 @@
 
-  <div class='pic-stream'>
+  <div className='pic-stream'>
     <ul>
       <li><img src= 'https://c1.staticflickr.com/3/2181/1568918513_b944560a2e_z.jpg?zz=1' /></li>
     </ul>
