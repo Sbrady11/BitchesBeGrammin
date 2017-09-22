1083c9bce72973d685be09eba80f88e78108c01a
Simon Brady <simonbrady@mac.com>
2017-09-22 07:47:12 -0400

    Rename it back to photo to save time, need to get back to that

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,45 @@
+class PhotosController < ApplicationController
+  
+  def index
+    @photo = Photo.all
+  end
+
+  def show
+  end
+  
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    if @photo = Photo.create(Photo_params)
+      flash[:success] = "photo added"
+      redirect_to Photos_path
+    else
+      flash.now[:alert] = "Error, please try again"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @photo.update(photo_params)
+      redirect_to photos_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @photo.destroy
+    redirect_to root_path
+  end
+  
+
+private
+  def photo_params
+    params.require(:photo).permit(:pic, :caption)
+  end
+end
